09e4b8b135be333bf23f9ef28013be740cf19ce6
Aaron Bach <bachya1208@gmail.com>
2019-05-23 09:28:02 -0600

    Better pip installation

--- init.sh
+++ init.sh
@@ -95,7 +95,7 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 
 echo ""
 echo "Installing Python packages..."
-sudo pip3 install \
+python3 -m pip install \
     "autopep8" \
     "flake8" \
     coverage \
